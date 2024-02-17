ed25c0d73ce4efb32976f659016eb6df2c6ae748
Mukesh <myadav986@rku.ac.in>
2024-02-17 21:32:45 +0530

    add env file

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,25 @@
 const connectDB = require('./db');
 const express = require('express');
-require('dotenv').config();
-
-// Use the promise returned by connectDB
-connectDB()
-  .then(() => {
-    console.log('Connected to MongoDB');
-
-    const app = express();
-    const port = 3000;
-
-    app.use(express.json());
-    
-    // Available Routes
-    app.use('/api/auth', require('./routes/auth'));
-    app.use('/api/notes', require('./routes/notes'));
-
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error(`MongoDB connection error: ${error}`);
-  });
 
 
-//   const connectDB = require('./db');
-
-// const express = require('express');
-// require('dotenv').config();
-
-// connectDB.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// connectDB.once('open', () => {
-//   console.log('Connected to MongoDB');
+connectDB.on('error', console.error.bind(console, 'MongoDB connection error:'));
+connectDB.once('open', () => {
+  console.log('Connected to MongoDB');
   
-//   const app = express();
-//   const port = 3000;
+  const app = express();
+  const port = 3000;
 
-//   app.use(express.json());
-//   // app.get('/', (req, res) => {
-//   //   res.send('Hello World!');
-//   // });
+  app.use(express.json());
+  // app.get('/', (req, res) => {
+  //   res.send('Hello World!');
+  // });
 
-//   //Available Routes
+  //Available Routes
 
-//   app.use('/api/auth',require('./routes/auth'));
-//   app.use('/api/notes',require('./routes/notes'));
+  app.use('/api/auth',require('./routes/auth'));
+  app.use('/api/notes',require('./routes/notes'));
 
-//   app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-//   });
-// });
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+});
