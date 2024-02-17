d8ac1f1ba1f81094f2898ba9de1b71c4d7e63673
Mukesh <myadav986@rku.ac.in>
2024-02-17 22:37:16 +0530

    create users

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ connectDB.once('open', () => {
   console.log('Connected to MongoDB');
   
   const app = express();
-  const port = 3000;
+  const port = 5000;
 
   app.use(express.json());
   // app.get('/', (req, res) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,24 +3,35 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-//Create a user using :post "api/auth/" .Doesnot require Auth
-router.post('/',[
+//Create a user using :post "api/auth/createuser" .Doesnot require Auth
+router.post('/createuser',[
     body('name','Enter a valid name').isLength({ min:3}),
     body('email','Enter a valid email').isEmail(),
     body('password','Pasword must be atleast 5 characters').isLength({ min: 5 }),
-],(req,res)=>{
+],async (req,res)=>{
+  //If there are errors , return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    // check whether the user with this exits already
+  try{
+      let user = await User.findOne({email: req.body.email});
+      if(user) {
+        return res.status(400).json({error:"User already exists"});
+    }
+    //create a new user
+    user=await User.create({
         name: req.body.name,
         email: req.body.email,
         password:req.body.password,
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err)
-    res.json({error:"please enter a unique email",message:err.message});
+      })
+      res.json(user)
+     }catch(error){
+      console.error(error.message);
+      res.status(500).send('Some error occurred');
+   }
     })
    
-})
+
 module.exports = router
